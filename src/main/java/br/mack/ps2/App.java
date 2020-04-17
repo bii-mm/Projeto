b310d6031529ecb40e96730d1dc2efe51843786d
Marcelaxz <marcela590@outlook.com>
2020-04-16 21:07:47 -0300

    Switch

--- src/main/java/br/mack/ps2/App.java
+++ src/main/java/br/mack/ps2/App.java
@@ -3,13 +3,43 @@ package br.mack.ps2;
 import br.mack.ps2.persistencia.ContaBancariaDAOMySQL;
 
 import java.sql.*;
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        ContaBancariaDAOMySQL mysqlDAO = new ContaBancariaDAOMySQL();
-        InterfaceUsuario interfaceUsuario = new InterfaceUsuario(mysqlDAO);
-        interfaceUsuario.iniciar();
+        int opc = 0;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("\n======== Menu ========");
+            System.out.println("\t1. Acessar Banco de Dados de Conta Bancária");
+            System.out.println("\t2. Acessar Banco de Dados de Empregado");
+            System.out.println("\t3. Acessar Banco de Dados de Jogo");
+            System.out.println("\t4. Sair");
+            System.out.print("Escolha uma opção:_ ");
+            opc = in.nextInt();
+
+            in.nextLine();
+
+            switch (opc) {
+                case 1:
+                    ContaBancariaDAOMySQL mysqlDAO = new ContaBancariaDAOMySQL();
+                    InterfaceUsuario interfaceUsuario = new InterfaceUsuario(mysqlDAO);
+                    interfaceUsuario.iniciar();
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Operação finalizada");
+                    break;
+                default:
+                    System.out.println("Opção Inválida");
+                    break;
+            }
+
+        }while (opc != 4);
+
     }
 }
